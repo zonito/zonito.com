0ae27fc506974c90db055515a9bea7acd4c8f44b
Love Sharma <zonito@protonmail.com>
2021-11-06 12:34:28 +0800

    Cupress Fix

--- cypress/integration/routing_layer_spec.ts
+++ cypress/integration/routing_layer_spec.ts
@@ -1,12 +1,12 @@
 /// <reference types="cypress" />
 /// <reference types="@testing-library/cypress" />
 
-describe('Security checklist', () => {
+describe('Routing Layer', () => {
   before(() => {
     cy.visit('/routinglayer')
   })
 
   it('should render page', () => {
-    cy.get('[data-cy="security-checklist"]').should('be.visible')
+    cy.get('[data-cy="routing-layer"]').should('be.visible')
   })
 })
