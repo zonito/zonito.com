073a6b298482427a594f12efccc3bd0b6527feb9
Love Sharma <zonito@protonmail.com>
2021-11-13 23:33:45 +0800

    Intro Update

--- src/components/Home/about.ts
+++ src/components/Home/about.ts
@@ -1,11 +1,15 @@
 export const about = `
-Hey, I'm Love Sharma. This is Personal Website; I intend to showcase my work, thoughts, and a little about personal things.
+Hey, I'm Love Sharma. It's my personal website; I intend to showcase my work, thoughts, and a little about personal things.
 
-I'm currently working with Autodesk, Singapore as a Principal Software Engineer. I worked on various projects and currently working on User Management System, a.k.a Identity.
+I'm currently working with Autodesk, Singapore as a Principal Software Engineer. Projects I've worked on include:
+
+- **Identity (Current)**: Autodesk's User management system manages around 23M userbase.
+- **API Management**: It's an API Management Service that manages Authentication, Authorisation, Rate Limiting, Monetization, Subscription, Locale, etc., capabilities as part of the overall platform. The foundational service is [Apigee](https://cloud.google.com/apigee). It handles ~7,000 TPS.
+- **Centralize Log Routing**: This Log Routing solution receives Application, Metrics, and Trace Logs from Different Services with capabilities like PII Masking, Truncate, Quota, etc., and routes them to different destinations like Splunk, S3, etc. The Scale is ~50GB/minute.
 
 Before Autodesk, I worked with Google, India, to build a training enrollment system for DoubleClick Customers, [Google Tag Manager](https://chrome.google.com/webstore/detail/tag-assistant-legacy-by-g/kejbdjndbnbjgmefkgdddjlbokphdefk) (Used by more than 2M+ Users) and a few more internal tools.
 
-I'm actively involved in a few StartUps (i.e., [Prediction Guru](https://www.prediction.guru/), [PolySport](https://www.polysports.org/), and a few more.) as a Technical Advisor.
+I'm actively involved in a few StartUps (i.e., [Prediction Guru](https://www.prediction.guru/), [Centre For Mindfulness](https://www.centreformindfulness.in/), [PolySport](https://www.polysports.org/), and a few more.) as a Technical Advisor / Consultant.
 
 My area of interest are:
 
