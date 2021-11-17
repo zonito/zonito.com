053fa1fdd09fe30375ddf8a3f40b744bdf548bc6
Love Sharma <zonito@protonmail.com>
2021-11-18 00:03:52 +0800

    Company Details

--- src/components/Home/Intro.tsx
+++ src/components/Home/Intro.tsx
@@ -28,7 +28,7 @@ interface TableRowProps {
 
 function TableRow({ href, title, subtitle, date }: TableRowProps) {
   return (
-    <a href={href} className="flex items-center space-x-4 group">
+    <a href={href} target="_blank" className="flex items-center space-x-4 group">
       <span className="flex-none font-medium text-gray-1000 group-hover:underline group-hover:text-blue-600 dark:group-hover:text-blue-500 dark:text-gray-100">
         {title}
       </span>
@@ -49,6 +49,35 @@ function SectionContainer(props) {
     />
   )
 }
+
+const technicalAdvisor = [
+  {
+    href: 'https://www.centreformindfulness.sg/',
+    company: 'Centre for Mindfulness',
+    title: 'Technical Advisor',
+    date: '2020-\u00a0\u00a0'
+  }, {
+    href: 'https://www.positiveconsulting.sg/',
+    company: 'Positive Performance Consulting',
+    title: 'Technical Consultant',
+    date: '2020-2020'
+  }, {
+    href: 'http://www.prediction.guru',
+    company: 'Prediction Guru',
+    title: 'Founding Member',
+    date: '2014-2020'
+  }, {
+    href: 'http://www.polysports.org',
+    company: 'Polysports',
+    title: 'Technical Consultant',
+    date: '2021-\u00a0\u00a0'
+  }, {
+    href: 'http://www.thehinduhub.org',
+    company: 'NGO: TheHinduHub',
+    title: 'Technical Consultant',
+    date: '2014-2020'
+  }
+]
 
 const workHistory = [
   {
@@ -73,7 +102,7 @@ const workHistory = [
     href: 'https://www.linkedin.com/company/patni/',
     title: 'Patni Computer Systems Ltd',
     subtitle: 'Software Developer',
-    date: '2008—2010==09',
+    date: '2008—2009',
   },
 ]
 
@@ -115,6 +144,23 @@ export function Intro() {
                     href={job.href}
                     title={job.title}
                     subtitle={job.subtitle}
+                    date={job.date}
+                    key={job.href}
+                  />
+                ))}
+              </div>
+            </SectionContent>
+          </SectionContainer>
+
+          <SectionContainer>
+            <SectionTitle>Consultant</SectionTitle>
+            <SectionContent>
+              <div className="flex flex-col space-y-3">
+                {technicalAdvisor.map((job) => (
+                  <TableRow
+                    href={job.href}
+                    title={job.company}
+                    subtitle={job.title}
                     date={job.date}
                     key={job.href}
                   />
